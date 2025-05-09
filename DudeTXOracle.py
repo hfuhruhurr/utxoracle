8c7e33ef489c09b8cde6d097eb1fc5dde5cdf94d
Doug Devine <dougdevine@gmail.com>
2025-05-09 12:47:10 -0400

    Finished Part 3

--- DudeTXOracle.py
+++ DudeTXOracle.py
@@ -1,6 +1,11 @@
 import os
 import subprocess
 import sys
+from datetime import datetime, timezone, timedelta
+import json 
+
+
+NUM_BLOCKS_FOR_CONSENSUS = 6
 
 def get_cli_options() -> list[str]:
     # Get the CLI options.
@@ -17,7 +22,7 @@ def ask_node(command: list[str]) -> None:
     bitcoin_cli_options = get_cli_options()
     full_command = ["bitcoin-cli"] + bitcoin_cli_options + command
     
-    print(f"Issuing bitcoin-cli command: {command}")
+    # print(f"Issuing bitcoin-cli command: {command}")
     try:
         response = subprocess.check_output(full_command)
         return response.decode().strip()
@@ -33,7 +38,40 @@ def ask_node(command: list[str]) -> None:
 if __name__ == '__main__':
     print('-' * 80)
     print('Let\'s go!')
-    block_hash = ask_node(['getblockhash', '777777'])
-    response = ask_node(['getblockheader', block_hash, 'true'])
-    print(f"type: {type(response)}")
-    print(response)
+    print('-' * 80)
+
+    # -------------------------------------------------------------------------
+    # Part 3: Get the latest consensus block to determine the latest price
+    #         we can use.
+    #
+    #         We define "latest" as one day before the consensus block.
+    # -------------------------------------------------------------------------
+    print("Part 3...")
+    consensus_block_height = int(ask_node(['getblockcount'])) - NUM_BLOCKS_FOR_CONSENSUS
+    consensus_block_hash = ask_node(['getblockhash', str(consensus_block_height)])
+    consensus_block_header = json.loads(ask_node(['getblockheader', consensus_block_hash, 'true']))
+    consensus_datetime = datetime.fromtimestamp(consensus_block_header['time'], tz=timezone.utc)
+    consensus_midnight = consensus_datetime.replace(hour=0, minute=0, second=0, microsecond=0)
+    latest_price_datetime = consensus_datetime - timedelta(days=1)
+
+    # print(f"    consensus_block_height: {consensus_block_height}")
+    # print(f"    consensus_block_hash  : {consensus_block_hash}")
+    # print(f"    consensus_block_header: {consensus_block_header}")
+    # print(f"    consensus_datetime    : {consensus_datetime}")
+    # print(f"    consensus_midnight    : {consensus_midnight}")
+    # print(f"    latest_price_datetime : {latest_price_datetime}")
+    
+    # -------------------------------------------------------------------------
+    # Part 4: If the user supplied a data argument, proccess it.
+    # -------------------------------------------------------------------------
+    print("Part 4...")
+
+    print(datetime(2025, 5, 8, 0, 0, 0, tzinfo=timezone.utc))
+    print(datetime(2025, 5, 8, 0, 0, 0, tzinfo=timezone.utc).timestamp())
+    print(consensus_midnight)
+    print(consensus_midnight - timedelta(days=1))
+
+    print('-' * 80)
+    print('Done!')
+    print('-' * 80)
+    
