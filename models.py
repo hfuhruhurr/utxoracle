9eaad4400d1b94b71f698f50dfbeff7ff5717efc
Doug Devine <dougdevine@gmail.com>
2025-05-13 18:15:12 -0400

    pre-proofed full raw block read

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ from typing import BinaryIO
 from dataclasses import dataclass
 from datetime import datetime, timezone
 from hashlib import sha256
-from typing import Tuple
+from typing import Tuple, List, Optional
 
 # invaluable resource: https://learnmeabitcoin.com/technical/block/blkdat/
 MAINNET_MAGIC_BYTES = b'\xf9\xbe\xb4\xd9'
@@ -27,81 +27,41 @@ class BlockHeader:
         """Get the block hash by hashing the header twice with SHA-256."""
         return hash256(self.header)[::-1]
 
-# @dataclass
-# class Input:
-#     txid: bytes
-#     vout_bytes: bytes
-#     script_size_bytes: bytes
-#     script: bytes
-#     sequence: bytes
+@dataclass
+class Input:
+    txid: bytes
+    vout: int
+    script_size: int
+    script: bytes
+    sequence: bytes
 
-#     @property
-#     def vout(self) -> int:
-#         """Get the output index as an integer."""
-#         return int.from_bytes(self.vout_bytes, byteorder='little')
-    
-#     @property
-#     def script_size(self) -> int:
-#         """Get the script size as an integer."""
-#         return int.from_bytes(self.script_size_bytes, byteorder='little')
-    
-# @dataclass
-# class Output:
-#     amount_bytes: bytes
-#     script_size_bytes: bytes
-#     script: bytes
+@dataclass
+class Output:
+    amount: int
+    script_size: int
+    script: bytes
 
-#     @property
-#     def amount(self) -> int:
-#         """Get the output amount as an integer."""
-#         return int.from_bytes(self.amount_bytes, byteorder='little')
-    
-#     @property
-#     def script_size(self) -> int:
-#         """Get the script size as an integer."""
-#         return int.from_bytes(self.script_size_bytes, byteorder='little')
+@dataclass
+class StackItem:
+    size: int
+    data: bytes
 
-# @dataclass
-# class StackItem:
-#     size_bytes: bytes
-#     data: bytes
+@dataclass
+class WitnessField:
+    n_stack_items: int
+    stack_items: List[StackItem]
 
-#     @property
-#     def size(self) -> int:
-#         """Get the size of the stack item as an integer."""
-#         return int.from_bytes(self.size_bytes, byteorder='little')
-    
-# @dataclass
-# class WitnessField:
-#     n_stack_items_bytes: bytes
-#     stack_items: List[StackItem]
-
-#     @property
-#     def n_stack_items(self) -> int:
-#         """Get the number of stack items as an integer."""
-#         return int.from_bytes(self.n_stack_items_bytes, byteorder='little')
-
-# @dataclass
-# class Transaction:
-#     version: bytes
-#     marker: Optional[bytes] 
-#     flag: Optional[bytes]
-#     n_inputs_bytes: bytes
-#     inputs: List[Input]
-#     n_outputs_bytes: bytes
-#     outputs: List[Output]
-#     witness: Optional[List[WitnessField]]  # Optional, only if marker == b'\x00'
-#     lock_time: bytes
-
-#     @property
-#     def n_inputs(self) -> int:
-#         """Get the number of inputs as an integer."""
-#         return int.from_bytes(self.n_inputs_bytes, byteorder='little')
-    
-#     @property
-#     def n_outputs(self) -> int:
-#         """Get the number of outputs as an integer."""
-#         return int.from_bytes(self.n_outputs_bytes, byteorder='little')
+@dataclass
+class Transaction:
+    version: bytes
+    marker: Optional[bytes] 
+    flag: Optional[bytes]
+    n_inputs: int
+    inputs: List[Input]
+    n_outputs: int
+    outputs: List[Output]
+    witness: Optional[List[WitnessField]] 
+    lock_time: bytes
 
 @dataclass
 class RawBlock:
@@ -152,7 +112,8 @@ class RawBlock:
         n_txs, tx_data_pos = cls.get_compact_size(block_data, pos=80)
 
         # Transaction data (from tx_data_pos to end)
-        txs = block_data[tx_data_pos:]
+        tx_data = block_data[tx_data_pos:]
+        txs = cls.parse_txs(tx_data)
 
         return RawBlock(
             magic_bytes=magic_bytes,
@@ -223,76 +184,120 @@ class RawBlock:
         else:
             return int.from_bytes(byte_data[pos+1:pos+9], 'little'), pos + 9
         
-    # @classmethod
-    # def parse_txs(cls, tx_data: bytes) -> List[Transaction]:
-    #     """Parse the transaction data into a list of Transaction objects."""
-    #     transactions = []
-    #     pos = 0
-    #     while pos < len(tx_data):
-    #         # Read transaction version
-    #         version = tx_data[pos:pos+4]
-    #         pos += 4
+    @staticmethod
+    def parse_txs(tx_data: bytes) -> List[Transaction]:
+        """Parse the transaction data into a list of Transaction objects."""
+        transactions = []
+        pos = 0
+        while pos < len(tx_data):
+            # Read transaction version
+            version = tx_data[pos:pos+4]
+            pos += 4
 
-    #         # Read marker and flag (if present)
-    #         if tx_data[pos:pos+1] == b'\x00':
-    #             marker = tx_data[pos:pos+1]
-    #             flag = tx_data[pos+1:pos+2]
-    #             pos += 2
-    #         else:
-    #             marker = None
-    #             flag = None
+            # Read marker and flag (if present)
+            if tx_data[pos:pos+1] == b'\x00':
+                marker = tx_data[pos:pos+1]
+                flag = tx_data[pos+1:pos+2]
+                pos += 2
+            else:
+                marker = None
+                flag = None
             
-    #         # Read number of inputs
-    #         n_inputs, pos = get_compact_size(tx_data, pos)
+            # Read number of inputs
+            n_inputs, pos = RawBlock.get_compact_size(tx_data, pos)
 
-    #         # Read inputs
-    #         inputs = tx_data[pos:pos+n_inputs]
-    #         pos += n_inputs
+            # Read inputs
+            inputs = []
+            for _ in range(n_inputs):
+                # Read txid (32 bytes)
+                txid = tx_data[pos:pos+32]
+                pos += 32
 
-    #         # Read number of outputs
-    #         n_outputs, pos = get_compact_size(tx_data, pos)
+                # Read vout (4 bytes)
+                vout_bytes = tx_data[pos:pos+4]
+                vout = int.from_bytes(vout_bytes, byteorder='little')
+                pos += 4
 
-    #         # Read outputs
-    #         outputs = tx_data[pos:pos+n_outputs]
-    #         pos += n_outputs
+                # Read script size (compact size integer)
+                script_size, pos = RawBlock.get_compact_size(tx_data, pos)
 
-    #         # Read witness (if present)
-    #         # There is a set of stack_items for each input
-    #         # Each stack_item is:
-    #         #     - a compact size integer (size of the upcoming item)
-    #         #     - the item data
-    #         if marker == b'\x00':
-    #             for i in range(n_inputs):
-    #                 # Read the number of stack items
-    #                 n_stack_items, pos = get_compact_size(tx_data, pos)
-    #                 # Read each stack item
-    #                 for j in range(n_stack_items):
-    #                     len_stack_item, pos = get_compact_size(tx_data, pos)
-    #                     stack_item = tx_data[pos:pos+len_stack_item]
-    #                     pos += len_stack_item
-    #         else:
-    #             witness = None
+                # Read script (variable length)
+                script = tx_data[pos:pos+script_size]
+                pos += script_size
 
-    #         # Read lock time
-    #         lock_time = tx_data[pos:pos+4]
-    #         pos += 4
+                # Read sequence (4 bytes)
+                sequence = tx_data[pos:pos+4]
+                pos += 4
 
-    #         transactions.append(Transaction(
-    #             version=version,
-    #             marker=marker,
-    #             flag=flag,
-    #             n_inputs=n_inputs, 
-    #             inputs=inputs, 
-    #             n_outputs=n_outputs, 
-    #             outputs=outputs,
-    #             witness=witness,
-    #             lock_time=lock_time
-    #         ))
+                # Create input object
+                inputs.append(Input(txid, vout, script_size, script, sequence))
 
-    #     return transactions
+            # Read number of outputs
+            n_outputs, pos = RawBlock.get_compact_size(tx_data, pos)
+
+            # Read outputs
+            outputs = []
+            for _ in range(n_outputs):
+                # Read amount (8 bytes)
+                amount_bytes = tx_data[pos:pos+8]
+                amount = int.from_bytes(amount_bytes, byteorder='little')
+                pos += 8
+
+                # Read script size (compact size integer)
+                script_size, pos = RawBlock.get_compact_size(tx_data, pos)
+
+                # Read script (variable length)
+                script = tx_data[pos:pos+script_size]
+                pos += script_size
+
+                # Create output object
+                outputs.append(Output(amount, script_size, script))
+
+            # Read witness (if present)
+            # There is a set of stack_items for each input
+            # Each stack_item is:
+            #     - a compact size integer (size of the upcoming item)
+            #     - the item data
+            if marker == b'\x00':
+                witness_fields = []
+                for _ in range(n_inputs):
+                    # Read the number of stack items
+                    n_stack_items, pos = RawBlock.get_compact_size(tx_data, pos)
+                    
+                    # Read each stack item
+                    stack_items = []
+                    for _ in range(n_stack_items):
+                        stack_item_size, pos = RawBlock.get_compact_size(tx_data, pos)
+                        stack_item = tx_data[pos:pos+stack_item_size]
+                        pos += stack_item_size
+                        stack_items.append(StackItem(stack_item_size, stack_item))
+                    
+                    witness_fields.append(WitnessField(n_stack_items, stack_items))
+                
+                witness = witness_fields
+            else:
+                witness = None
+
+            # Read lock time
+            lock_time = tx_data[pos:pos+4]
+            pos += 4
+
+            transactions.append(Transaction(
+                version=version,
+                marker=marker,
+                flag=flag,
+                n_inputs=n_inputs,
+                inputs=inputs,
+                n_outputs=n_outputs,
+                outputs=outputs,
+                witness=witness,
+                lock_time=lock_time
+            ))
+
+        return transactions
     
     def __repr__(self) -> str:
-        return (
+        result = (
             f"    Is mainnet?                : {self.is_mainnet}\n"
             f"    Block size                 : {self.size:,} bytes = {self.size / 1000 / 1000:.02f} MB\n"
             f"    Block header               : {self.block_header.header.hex()[:40]}...{self.block_header.header.hex()[-40:]}\n"
@@ -304,7 +309,21 @@ class RawBlock:
             f"      Nonce                    : {self.block_header.nonce[::-1].hex()}\n"
             f"    Block hash                 : {self.block_header.block_hash.hex()}\n"
             f"    # of txs                   : {self.n_txs:,}\n"
-            f"    Tx data (truncated)        : {self.txs.hex()[:40]}...{self.txs.hex()[-40:]}\n"
-            # f"\n    len(txs) = {len(self.txs)}, type(txs) = {type(self.txs)}"
-            "\n"
         )
+        
+        result += "\n    Transactions:\n"
+        for i, tx in enumerate(self.txs, 1):
+            if i <= 3:
+                result += (
+                    f"    --------------------------\n"
+                    f"    Transaction #{i}:\n"
+                    f"      Version                 : {tx.version[::-1].hex()}\n"
+                    f"      Marker                  : {tx.marker[::-1].hex() if tx.marker else 'None'}\n"
+                    f"      Flag                    : {tx.flag[::-1].hex() if tx.flag else 'None'}\n"
+                    f"      # inputs                : {tx.n_inputs:,}\n"
+                    f"      # outputs               : {tx.n_outputs:,}\n"
+                    f"      # witness fields        : {len(tx.witness) if tx.witness else 0}\n"
+                    f"      Lock time               : {tx.lock_time[::-1].hex()}\n"
+                )
+        return result + "\n"
+        
